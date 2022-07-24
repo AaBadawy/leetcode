4bb6e7d4c9eace2b829a7088ffb17ede16190ce4
Ahmed Badawy <63416196+AaBadawy@users.noreply.github.com>
2022-07-25 01:23:17 +0200

    Time: 121 ms (34.82%), Space: 30.7 MB (70.37%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.php
@@ -0,0 +1,41 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return NULL
+     */
+    function rotate(&$nums, $k) {
+        $total = count($nums);
+        
+        $end_pointer = $total -1;
+        
+        $start_pointer = 0;
+        
+        $result = $nums;
+        
+        $rotates = $k;
+        
+        if($k > $total)
+            $rotates = $k % $total;
+        
+        while($rotates > 0) {
+            $nums[$rotates - 1] = $result[$end_pointer];
+            
+            $end_pointer -- ;
+            
+            $rotates--;
+        }
+        for($i = $k % $total; $i < $total; $i ++){
+            
+            $nums[$i] = $result[$start_pointer];
+            
+            $start_pointer ++;
+        }
+        return null;
+        
+        return null;
+        
+        
+    }
+}
